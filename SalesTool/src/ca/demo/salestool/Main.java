41e06aaf9d31b8f952f79d98837e5f1dbe43997f
unlimiteddigits <ericmw02@students.ipfw.edu>
2016-10-06 20:29:45 -0400

    initial checkin

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,15 @@
+package ca.demo.salestool;
+//before commit
+public class Main {
+	public static void main(String[] args){
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+
+	private static void displayGreeting() {
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This app shows Sales Data");
+	}
+}
