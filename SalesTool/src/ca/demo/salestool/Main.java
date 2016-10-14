c4513b1d3215c06d9f99114fe635cb1d4fc030d7
unlimiteddigits <ericmw02@students.ipfw.edu>
2016-10-13 22:04:51 -0400

    Test 5-from eclipse had to pull from upstream into current branch

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -17,6 +17,7 @@ public class Main {
 		System.out.println("Test 2-from web");
 		System.out.println("Test 3-from eclipse after rebase");
 		System.out.println("Test 4-from web to test pull from upstream");
+		System.out.println("Test 5-from eclipse had to pull from upstream into current branch");
 		
 	}
 }
