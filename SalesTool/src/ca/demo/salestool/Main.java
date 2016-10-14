938e1d2d8f5870966152fd4ab8d14c5ce8bddf93
unlimiteddigits <ericmw02@students.ipfw.edu>
2016-10-13 22:10:53 -0400

    Test 7-from eclipse to test changes at two locations

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -18,6 +18,6 @@ public class Main {
 		System.out.println("Test 3-from eclipse after rebase");
 		System.out.println("Test 4-from web to test pull from upstream");
 		System.out.println("Test 5-from eclipse had to pull from upstream into current branch");
-		
+		System.out.println("Test 7-from eclipse to test changes at two locations");		
 	}
 }
